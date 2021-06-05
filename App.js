b4bb727c3c13b53ea5f34e42978b85c627f3a55e
ALVIN THOMAS <alvinthomas206@gmail.com>
2021-06-05 08:58:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createAppContainer, createSwitchNavigator} from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import WelcomeScreen from './screens/WelcomeScreen';
+
+
+export default function App() {
+  return (
+   <WelcomeScreen/>
+  );
+}
+
+
